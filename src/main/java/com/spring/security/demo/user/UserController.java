202bc42c350ad88756496c095579a2a094d76149
BSC-COM-07-19 <bsc-com-07-19@unima.ac.mw>
2022-10-05 23:02:12 +0200

    Done with question 1(disabling),2(updating) and 3(viewing)

--- src/main/java/com/spring/security/demo/user/UserController.java
+++ src/main/java/com/spring/security/demo/user/UserController.java
@@ -7,10 +7,12 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
@@ -32,6 +34,7 @@ public class UserController {
     }
 
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') and hasRole('ROLE_EXECUTIVE')")
     @GetMapping
     public ResponseEntity<PagedModel<?>> getAllUsers(@PositiveOrZero  @RequestParam(value="page", defaultValue = "0") int page,
                                                       @Positive @RequestParam(value="size", defaultValue = "20") int size,
@@ -44,6 +47,7 @@ public class UserController {
         return userService.getUser(authentication);
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_EXECUTIVE"})
     @PutMapping("update-role/{userName}/{role}")
     public ResponseEntity<ApiResponse> updateUserRole(@PathVariable("userName") String userName,
                                                       @PathVariable("role") String role,
@@ -51,6 +55,7 @@ public class UserController {
         return userService.updateUserRole(userName, role, currentUser);
     }
 
+    @Secured("ROLE_ADMIN")
     @PutMapping("disable-account/{userName}")
     public ResponseEntity<ApiResponse> disableAccount(@PathVariable("userName") String userName){
         return userService.disableAccount(userName);
